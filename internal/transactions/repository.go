2115c6bc0f5e6a8efedff45f6c28e9c18baab4fe
Xartyago <santiago.salcedo@mercadolibre.com.co>
2022-07-07 16:32:37 -0500

    Second Commit

--- internal/transactions/repository.go
+++ internal/transactions/repository.go
@@ -1,13 +1,34 @@
 package transactions
 
+import "github.com/Xartyago/DDD/internal/domain"
+
 type Repository interface {
-	GetAll()
-	Store()
+	GetAll() ([]domain.Transaction, error)
+	Store(id int, transactionCode, currency, emisor, receiver, transactionDate string, amount float64) (domain.Transaction, error)
 }
 
 type repository struct{}
 
+var ts []domain.Transaction
+
 func NewRepository() Repository {
-	// return &repository{}
-	return NewRepository()
+	return &repository{}
+}
+
+func (r *repository) GetAll() ([]domain.Transaction, error) {
+	return ts, nil
+}
+
+func (r *repository) Store(id int, transactionCode, currency, emisor, receiver, transactionDate string, amount float64) (domain.Transaction, error) {
+	newTs := domain.Transaction{
+		Id:              id,
+		TransactionCode: transactionCode,
+		Currency:        currency,
+		Emisor:          emisor,
+		Receiver:        receiver,
+		TransactionDate: transactionDate,
+		Amount:          amount,
+	}
+	ts = append(ts, newTs)
+	return newTs, nil
 }
--- internal/transactions/service.go
+++ internal/transactions/service.go
@@ -1,1 +1,36 @@
 package transactions
+
+import "github.com/Xartyago/DDD/internal/domain"
+
+type Service interface {
+	GetAll() ([]domain.Transaction, error)
+	Store(id int, transactionCode, currency, emisor, receiver, transactionDate string, amount float64) (domain.Transaction, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(r Repository) Service {
+	return &service{
+		repository: r,
+	}
+}
+func (s *service) GetAll() ([]domain.Transaction, error) {
+	ts, err := s.repository.GetAll()
+	if err != nil {
+		return nil, err
+	}
+	return ts, nil
+}
+
+func (s *service) Store(id int, transactionCode, currency, emisor, receiver, transactionDate string, amount float64) (domain.Transaction, error) {
+	newTs, err := s.repository.Store(
+		id, transactionCode, transactionDate, currency, emisor, receiver, amount,
+	)
+	if err != nil {
+		return domain.Transaction{}, err
+	}
+	ts = append(ts, newTs)
+	return newTs, nil
+}
